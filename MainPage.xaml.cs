2d1b93612f4a075d9f4de369ec270fd729487468
Alejo Kim Uy <alejokimuy@gmail.com>
2024-10-15 09:51:38 -0600

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LabEventDrivenApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
